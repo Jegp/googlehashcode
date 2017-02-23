0b83f995ed3fbf9fed58c8d83953fac8a22f4b27
Martin Bech <martin@yacc.io>
2017-02-23 19:27:12 +0100

    program

---
+++ program.py
@@ -0,0 +1,6 @@
+data_center_capacity = 9999999999999999999999999
+
+caches = {"c0": (data_center_capacity, [])}
+endpoints = {"e0": [(10, "v0")]}
+videos = {"v0": 100}
+latency = {("e0", "c0"): 1000}
