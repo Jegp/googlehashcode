cc01ca72a47b8d25111f1eb3b61e7890228f8718
Jens Egholm <jens@selftiesoftware.com>
2017-02-23 21:20:07 +0100

    Made a bash script for running the files

---
+++ compute.sh
@@ -0,0 +1,5 @@
+python program.py me_at_the_zoo.in > me.out
+python program.py trending_today.in > trending.out
+python program.py videos_worth_spreading.in > videos.out
+python program.py kittens.in > kittens.out
+
